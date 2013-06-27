b855a198d953d5e932fdbb16dafea1f5781c9030
Caduri Katzav <cadurik@gmail.com>
2013-06-27 22:21:55 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BubbleTalk::Application.config.secret_token = '9d31fafbffeff0f9c12fd4b44f46f656ddf76d8e92f88552051f76afe82c46ae376546776af3f94993482a3ac215deb111e431d2d02a18162b7cffb56871e0eb'
